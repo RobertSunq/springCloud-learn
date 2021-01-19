50c43b7904c0a1b15f4f0501723442957572dcc2
sunqingbao <lhbj_sunqb@cpnc.com.cn>
2021-01-19 21:04:35 +0800

    支付模块初始化构建-1

---
+++ code/springcloud/cloud-provider-payment18001/src/main/java/com/qing/springcloud/PaymentMain18001.java
@@ -0,0 +1,19 @@
+package com.qing.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: sunQB
+ * @Date: 2021-01-19 20:59
+ * @Since: JDK-1.8
+ */
+
+@SpringBootApplication
+public class PaymentMain18001 {
+
+    public static void main(String[] args){
+        SpringApplication.run(PaymentMain18001.class,args);
+
+    }
+}
